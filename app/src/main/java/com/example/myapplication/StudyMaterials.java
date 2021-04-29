5b70d12293ccd88240caffa19ba50893af694b70
Vedant Wani <vedantwani788@gmail.com>
2021-04-29 12:02:59 +0530

    Created a rough initial template for switching from "Our Activity" to the tabbed activity on button click

---
+++ app/src/main/java/com/example/myapplication/StudyMaterials.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class StudyMaterials extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.study_materials_layout,container,false);
+    }
+}
